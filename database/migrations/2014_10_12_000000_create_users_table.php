534f904e7b38f50df28bf54a580192e772000672
Tung <tung@test.com>
2020-10-03 10:59:39 +0700

    update user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -24,8 +24,8 @@ class CreateUsersTable extends Migration
             $table->rememberToken();
             $table->timestamps();
         });
-        $pwd = bcrypt("12345678");
-        DB::table('users')->insert([['id' => 1, 'name' => 'Admin', 'email' => 'admin@lavina.com', 'username' => 'admin', 'password' => $pwd, 'role_id' => "1", 'is_active' => true], ['id' => 2, 'name' => 'Business', 'email' => 'business@lavina.com', 'username' => 'business', 'password' => $pwd, 'role_id' => "2", 'is_active' => true], ['id' => 3, 'name' => 'Manager', 'email' => 'manager@lavina.com', 'username' => 'manager', 'password' => $pwd, 'role_id' => "3", 'is_active' => true], ['id' => 4, 'name' => 'Employee', 'email' => 'employee@lavina.com', 'username' => 'employee', 'password' => $pwd, 'role_id' => "4", 'is_active' => true]]);
+        // $pwd = bcrypt("12345678");
+        // DB::table('users')->insert([['id' => 1, 'name' => 'Admin', 'email' => 'admin@lavina.com', 'username' => 'admin', 'password' => $pwd, 'role_id' => "1", 'is_active' => true], ['id' => 2, 'name' => 'Business', 'email' => 'business@lavina.com', 'username' => 'business', 'password' => $pwd, 'role_id' => "2", 'is_active' => true], ['id' => 3, 'name' => 'Manager', 'email' => 'manager@lavina.com', 'username' => 'manager', 'password' => $pwd, 'role_id' => "3", 'is_active' => true], ['id' => 4, 'name' => 'Employee', 'email' => 'employee@lavina.com', 'username' => 'employee', 'password' => $pwd, 'role_id' => "4", 'is_active' => true]]);
     }
 
     /**
